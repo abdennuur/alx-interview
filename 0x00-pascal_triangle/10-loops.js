01cab7d2a125bd571d77ff510f054a8810abee49
abdennuur <abdnourkhatib@gmail.com>
2024-06-27 16:16:19 +0000

    del files

--- 0x00-pascal_triangle/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const nwArr = [];
-  for (const value of array) {
-    nwArr.push(appendString + value);
-  }
-
-  return nwArr;
-}
