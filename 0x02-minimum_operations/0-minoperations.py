36f7db4ea17f4d27aeeb096c2f2dcae1e266bee2
abdennuur <abdnourkhatib@gmail.com>
2024-07-11 21:31:13 +0000

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-'''Minimum operations coding challenge.
+'''The Min operations coding challenge.
 '''
 
 
 def minOperations(n):
     '''To compute the fewest nbr of operations needed to result
-    in exactly nu H characters.
+    in exactly n 'H' characters.
     '''
     if not isinstance(n, int):
         return 0
     ops_cnt = 0
     clipbrd = 0
     dn = 1
+    # to print('H', end='')
     while dn < n:
         if clipbrd == 0:
+            # to init (the first copy all and paste)
             clipbrd = dn
             dn += clipbrd
             ops_cnt += 2
+            # to print('-(11)->{}'.format('H' * done), end='')
         elif n - dn > 0 and (n - dn) % dn == 0:
-            clipbd = dn
+            # to copy all and paste
+            clipbrd = dn
             dn += clipbrd
             ops_cnt += 2
+            # to print('-(11)->{}'.format('H' * done), end='')
         elif clipbrd > 0:
+            # to paste
             dn += clipbrd
             ops_cnt += 1
+            # to print('-(01)->{}'.format('H' * done), end='')
+    # to print('')
     return ops_cnt
